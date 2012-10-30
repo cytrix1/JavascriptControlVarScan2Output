0282a06ddb9fc75cd38fb45f7ca01df4777a3b40
Young Soo Song <extramorein@gmail.com>
2012-10-30 16:58:12 +0900

    New files are created

---
+++ comparewithotherresearch.js
@@ -0,0 +1,3 @@
+var excApp = new ActiveXObject("Excel.Application"); 
+excApp.visible = true;
+var excBook = excApp.Workbooks.open("C:\\Users\\nam\\Dropbox\\Research projects\\Small Cell Lung Cancer 분석\\Smallcel임시.xls"); 
